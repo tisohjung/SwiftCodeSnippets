2ecc237231c08761c296b4e87ca554f59ccb5d1d
Minho Yi <minho@bitfinder.co>
2018-12-28 17:02:22 +0900

    updated to Swift 4.2

--- SwiftCodeSnippets/SwiftCodeSnippetsManager.swift
+++ SwiftCodeSnippets/SwiftCodeSnippetsManager.swift
@@ -13,9 +13,9 @@ let configureDirectory = "/Library/Application Support/SelfStudio"
 var sharedPlugin: SwiftCodeSnippetsManager?
 
 class SwiftCodeSnippetsManager: NSObject {
-    var bundle: NSBundle
-    let NTC = NSNotificationCenter.defaultCenter()
-    init(bundle: NSBundle) {
+    var bundle: Bundle
+    let NTC = NotificationCenter.default
+    init(bundle: Bundle) {
         self.bundle = bundle
         super.init()
         updateCodeSnippets()
@@ -29,101 +29,109 @@ class SwiftCodeSnippetsManager: NSObject {
 // MARK: - UpdateCodeSnippets
 extension SwiftCodeSnippetsManager {
     func updateCodeSnippets(){
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
-            let data = NSData(contentsOfURL: NSURL(string: directoryUrl)!)
-            if let aDat = data ,
-                let content = NSString(data: aDat, encoding: NSUTF8StringEncoding) as? String{
-                    let start = "<td class=\"content\">"
-                    let end = "</td>"
-                    let matches = content.matchesOf(start, endPattern: end)
-                    var urls = [String]()
-                    for item in matches{
-                        let hrefMatch = item.matchesOf("href=\"", endPattern: "\"")
-                        if hrefMatch.count > 0 {
-                            let url = hrefMatch[0]
-                            if (url as NSString).hasSuffix(".codesnippet") {
-                                urls.append(url)
-                            }
-                        }
-                    }
-                    let filePath = self.appConfigure()
-                    var toDownloads = [String]()
-                    var toDeletes = [String]()
-                    let nowArray = urls as NSArray
-                    if let oldArray = NSArray(contentsOfFile: filePath) {
-                        for item in oldArray {
-                            if !nowArray.containsObject(item) {
-                                toDeletes.append(item as! String)
-                            }
-                        }
-                        for item in nowArray {
-                            if !oldArray.containsObject(item) {
-                                toDownloads.append(item as! String)
-                            }
-                        }
-                    }
-                    
-                    if urls.count > 0 {
-                        nowArray.writeToFile(filePath, atomically: true)
-                    }
-                    
-                    if toDownloads.count > 0 {
-                        self.downloadFrom(toDownloads, done: { () -> () in
-                            print("😀😀😀😀😀😀SwiftCodeSnippetsManager updat done")
-                        })
-                    }else{
-                        print("😀😀😀😀😀😀SwiftCodeSnippetsManager not downloading")
-                    }
-                    
-                    if toDeletes.count > 0 {
-                        self.deleteFiles(toDeletes)
-                    }else{
-                        print("😀😀😀😀😀😀SwiftCodeSnippetsManager not delete files")
-                    }
-
-                    
+//        DispatchQueue.main.async {
+//        }
+        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async { () -> Void in
+            
+//        }
+//        dispatch_async(DispatchQueue.global(DispatchQueue.GlobalQueuePriority.default, 0), {
+            guard let data = try? Data(contentsOf: NSURL(string: directoryUrl)! as URL) else {
+                print("\(#file) \(#function)[39]: Error trying to create Data")
+                return
             }
-        })
+            if let content = String(data: data, encoding: String.Encoding.utf8) {
+                let start = "<td class=\"content\">"
+                let end = "</td>"
+                let matches = content.matchesOf(startPattern: start, endPattern: end)
+                var urls = [String]()
+                for item in matches{
+                    let hrefMatch = item.matchesOf(startPattern: "href=\"", endPattern: "\"")
+                    if hrefMatch.count > 0 {
+                        let url = hrefMatch[0]
+                        if (url as NSString).hasSuffix(".codesnippet") {
+                            urls.append(url)
+                        }
+                    }
+                }
+                let filePath = self.appConfigure()
+                var toDownloads = [String]()
+                var toDeletes = [String]()
+                let nowArray = urls as NSArray
+                if let oldArray = NSArray(contentsOfFile: filePath) {
+                    for item in oldArray {
+                        if !nowArray.contains(item) {
+                            toDeletes.append(item as! String)
+                        }
+                    }
+                    for item in nowArray {
+                        if !oldArray.contains(item) {
+                            toDownloads.append(item as! String)
+                        }
+                    }
+                }
+                
+                if urls.count > 0 {
+                    nowArray.write(toFile: filePath, atomically: true)
+                }
+                
+                if toDownloads.count > 0 {
+                    self.downloadFrom(urls: toDownloads, done: { () -> () in
+                        print("😀😀😀😀😀😀SwiftCodeSnippetsManager updat done")
+                    })
+                } else {
+                    print("😀😀😀😀😀😀SwiftCodeSnippetsManager not downloading")
+                }
+                
+                if toDeletes.count > 0 {
+                    self.deleteFiles(items: toDeletes)
+                } else {
+                    print("😀😀😀😀😀😀SwiftCodeSnippetsManager not delete files")
+                }
+                
+                
+            }
+        }
     }
     
     func appConfigure()->String{
-        let dir = (NSHomeDirectory() as NSString).stringByAppendingPathComponent(configureDirectory)
-        let fm = NSFileManager.defaultManager()
+        let dir = (NSHomeDirectory() as NSString).appendingPathComponent(configureDirectory)
+        let fm = FileManager.default
         var isDir : ObjCBool = true
-        if !fm.fileExistsAtPath(dir, isDirectory: &isDir) {
+        if !fm.fileExists(atPath: dir, isDirectory: &isDir) {
             do {
-                try fm.createDirectoryAtPath(dir, withIntermediateDirectories: false, attributes: nil)
+                try fm.createDirectory(atPath: dir, withIntermediateDirectories: false, attributes: nil)
             } catch _ {
             }
         }
-        let configureFile = (dir as NSString).stringByAppendingPathComponent("conf")
+        let configureFile = (dir as NSString).appendingPathComponent("conf")
         return configureFile
     }
     
     func deleteFiles(items:[String]){
         for item in items {
-            let fm = NSFileManager.defaultManager()
-            let home = (NSHomeDirectory() as NSString).stringByAppendingPathComponent(codeSnippetDirectory)
-            let path = (home as NSString).stringByAppendingPathComponent(item)
+            let fm = FileManager.default
+            let home = (NSHomeDirectory() as NSString).appendingPathComponent(codeSnippetDirectory)
+            let path = (home as NSString).appendingPathComponent(item)
             do {
-                try fm.removeItemAtPath(path)
+                try fm.removeItem(atPath: path)
             } catch _ {
             }
         }
     }
     
-    func downloadFrom(var urls:[String],done:()->()){
+    func downloadFrom(urls:[String], done:()->()){
+        var urls = urls
         if urls.count > 0 {
             if let alast = urls.last {
             var last = alast as NSString
-            last = last .stringByReplacingOccurrencesOfString("/blob", withString: "")
+                last = last .replacingOccurrences(of: "/blob", with: "") as NSString
             if let  aurl = NSURL(string: "https://raw.githubusercontent.com" + (last as String)) ,
-                let data = NSData(contentsOfURL: aurl){
+                let data = NSData(contentsOf: aurl as URL){
                     let fileName = alast.fileName()
-                    data.write(fileName)
+                data.write(name: fileName)
                     urls.removeLast()
                     if urls.count > 0 {
-                        downloadFrom(urls, done: done)
+                        downloadFrom(urls: urls, done: done)
                     }else{
                         done()
                     }
@@ -137,12 +145,12 @@ extension SwiftCodeSnippetsManager {
 
 extension NSData {
     func write(name:String){
-        let fm = NSFileManager.defaultManager()
-        let home = (NSHomeDirectory() as NSString).stringByAppendingPathComponent(codeSnippetDirectory)
-        let path = (home as NSString).stringByAppendingPathComponent(name)
+        let fm = FileManager.default
+        let home = (NSHomeDirectory() as NSString).appendingPathComponent(codeSnippetDirectory)
+        let path = (home as NSString).appendingPathComponent(name)
         var isDir : ObjCBool = false
-        if !fm.fileExistsAtPath(path, isDirectory: &isDir) {
-            self.writeToFile(path, atomically: true)
+        if !fm.fileExists(atPath: path, isDirectory: &isDir) {
+            self.write(toFile: path, atomically: true)
         }
     }
 }
@@ -152,20 +160,20 @@ extension String {
     }
     
     func matchesOf(startPattern:String,endPattern:String)->[String]{
-        let startRx = try? NSRegularExpression(pattern: startPattern, options: NSRegularExpressionOptions())
-        let endRx = try? NSRegularExpression(pattern: endPattern, options: NSRegularExpressionOptions())
+        let startRx = try? NSRegularExpression(pattern: startPattern, options: NSRegularExpression.Options())
+        let endRx = try? NSRegularExpression(pattern: endPattern, options: NSRegularExpression.Options())
         let selfLen = self.utf16.count
         let range = NSMakeRange(0, selfLen)
         var codeSnippets: [String]! = [String]()
-        startRx?.enumerateMatchesInString(self, options: NSMatchingOptions(), range: range, usingBlock: { (match, flags, stop) -> Void in
+        startRx?.enumerateMatches(in: self, options: NSRegularExpression.MatchingOptions(), range: range, using: { (match, flags, stop) -> Void in
             if let startMatch = match {
                 let startLen = startMatch.range.location + startMatch.range.length
                 let len = selfLen - startLen
                 let subRange = NSMakeRange(startLen, len)
-                if let endMatch = endRx?.firstMatchInString(self, options: NSMatchingOptions(), range: subRange) {
+                if let endMatch = endRx?.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(), range: subRange) {
                     let dataRangeLen = endMatch.range.location - startLen
                     let dataRange = NSMakeRange(startLen, dataRangeLen)
-                    let aData = (self as NSString).substringWithRange(dataRange)
+                    let aData = (self as NSString).substring(with: dataRange)
                     codeSnippets.append(aData)
                 }
             }
